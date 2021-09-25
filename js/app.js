0fa00cb993b68e2287065f9241898e8ffafbbf82
Aydan <aydan2903@gmail.com>
2021-09-25 22:40:17 +0400

    navbar clicked bg is ready

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 const miniBar = document.querySelector('.mini-navbar')
 const miniBarClose = document.querySelector('.mini-navbar-close')
+const listItem = document.querySelectorAll('.list-item')
 
 const open = document.querySelector('.fa-bars')
 const close = document.querySelector('.fa-times')
@@ -13,4 +14,8 @@ close.addEventListener('click', function() {
     close.classList.add('none')
     miniBarClose.classList.toggle('none')
     open.classList.remove('none')
+})
+
+listItem.addEventListener('click', function() {
+    listItem.classList.add('active')
 })
