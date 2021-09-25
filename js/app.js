b8c1a29dc621ddf1895d4bb03a52395b189c96d2
Aydan <aydan2903@gmail.com>
2021-09-25 21:58:54 +0400

    azcool new page

---
+++ js/app.js
@@ -0,0 +1,16 @@
+const miniBar = document.querySelector('.mini-navbar')
+const miniBarClose = document.querySelector('.mini-navbar-close')
+
+const open = document.querySelector('.fa-bars')
+const close = document.querySelector('.fa-times')
+
+open.addEventListener('click', function() {
+    open.classList.add('none')
+    miniBarClose.classList.toggle('none')
+    close.classList.remove('none')
+})
+close.addEventListener('click', function() {
+    close.classList.add('none')
+    miniBarClose.classList.toggle('none')
+    open.classList.remove('none')
+})
